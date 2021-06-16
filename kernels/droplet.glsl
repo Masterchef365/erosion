5e178c6fcb4fc2d24908853d5e8cb5ef6f688ac6
Masterchef365 <duncan.freeman1@gmail.com>
2021-06-16 14:31:13 -0700

    Rain

--- kernels/droplet.glsl
+++ kernels/droplet.glsl
@@ -1,3 +1,7 @@
+float rand(vec2 c){
+    return fract(sin(dot(c.xy, vec2(19.9898,71.233))) * 4758.5453);
+}
+
 struct Droplet {
     // Position
     vec2 pos;
@@ -10,3 +14,19 @@ struct Droplet {
     // Sediment
     float sediment;
 };
+
+Droplet init_droplet(vec2 seed) {
+    Droplet droplet;
+    droplet.pos = vec2(
+        rand(seed * vec2(23.123, 34.1241)),
+        rand(seed * vec2(95.123, 91.321))
+    );
+    droplet.dir = vec2(
+        rand(seed * vec2(13.13, 14.141)),
+        rand(seed * vec2(25.12, 21.31))
+    );
+    droplet.vel = 1.;
+    droplet.water = 1.;
+    droplet.sediment = 0.;
+    return droplet;
+}
